0a6fae051e82c44d907c230c4b39b2d9c021a936
Cherryboome <muhamidifarian@yahoo.com>
2019-10-29 09:39:02 +0200

    added logic to fetch api data for RecipeList and Recipe components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { recipes } from "./tempList";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
-function App() {
-  return (
-    <>
-      <RecipeList />
-      <RecipeDetails />
-    </>
-  );
+class App extends Component {
+  state = {
+    recipes: recipes,
+    url:
+      "https://www.food2fork.com/api/search?key=bc7130022842c928e51df2f917ca8696"
+  };
+
+  // async getRecipes() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
+
+  render() {
+    // console.log(this.state.recipes);
+
+    return (
+      <>
+        <RecipeList recipes={this.state.recipes} />
+        <RecipeDetails />
+      </>
+    );
+  }
 }
 
 export default App;
