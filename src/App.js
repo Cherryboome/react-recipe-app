f81c5012420220abe5098489627a164c2d0f5f87
Cherryboome <muhamidifarian@yahoo.com>
2019-10-29 12:57:34 +0200

    added logic to alternate between page list and detail views

--- src/App.js
+++ src/App.js
@@ -9,33 +9,63 @@ class App extends Component {
     recipes: recipes,
     url:
       "https://www.food2fork.com/api/search?key=bc7130022842c928e51df2f917ca8696",
-    details_id: 35380
+    details_id: 35380,
+    pageIndex: 1
   };
 
-  // async getRecipes() {
-  //   try {
-  //     const data = await fetch(this.state.url);
-  //     const jsonData = await data.json();
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
 
-  //     this.setState({
-  //       recipes: jsonData.recipes
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+      this.setState({
+        // recipes: jsonData.recipes
+        recipes: recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // componentDidMount() {
-  //   this.getRecipes();
-  // }
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = index => {
+    switch (index) {
+      default:
+      case 1:
+        return (
+          <RecipeList
+            recipes={this.state.recipes}
+            handleDetails={this.handleDetails}
+          />
+        );
+      case 0:
+        return (
+          <RecipeDetails
+            id={this.state.details_id}
+            handleIndex={this.handleIndex}
+          />
+        );
+    }
+  };
+
+  handleIndex = index => {
+    this.setState({
+      pageIndex: index
+    });
+  };
+
+  handleDetails = (index, id) => {
+    this.setState({
+      pageIndex: index,
+      details_id: id
+    });
+  };
 
   render() {
-    return (
-      <>
-        {/* <RecipeList recipes={this.state.recipes} /> */}
-        <RecipeDetails id={this.state.details_id} />
-      </>
-    );
+    return <>{this.displayPage(this.state.pageIndex)}</>;
   }
 }
 
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -37,7 +37,8 @@ export default class RecipeDetails extends Component {
 
       this.setState(
         (state, props) => {
-          return { recipe: jsonData.recipe };
+          // return { recipe: jsonData.recipe };
+          return { recipe: recipe };
         },
         () => {}
       );
@@ -56,6 +57,8 @@ export default class RecipeDetails extends Component {
       ingredients
     } = this.state.recipe;
 
+    const { handleIndex } = this.props;
+
     return (
       <>
         <div className='container'>
@@ -63,7 +66,8 @@ export default class RecipeDetails extends Component {
             <div className='col-10 mx-auto col-md-6 my-3'>
               <button
                 type='button'
-                className='btn btn-warning mb-5 text-capitalize'>
+                className='btn btn-warning mb-5 text-capitalize'
+                onClick={() => handleIndex(1)}>
                 back to recipe list
               </button>
               <img src={image_url} className='d-block w-100' alt='recipe' />
